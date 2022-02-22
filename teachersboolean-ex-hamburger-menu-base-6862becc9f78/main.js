b647fe06973ccacfc1f2c1663622efe564f8fc55
Marzio95 <marziodellarocca14@gmail.com>
2022-02-22 18:45:59 +0100

    provo soluzioni per far leggere la funzione

--- teachersboolean-ex-hamburger-menu-base-6862becc9f78/main.js
+++ teachersboolean-ex-hamburger-menu-base-6862becc9f78/main.js
@@ -1,4 +1,14 @@
-const hamburgerButton = document.getElementById('close')
-hamburgerButton.addEventListener('click', function(){
-    
-}
+let element = document.querySelector('header-right > a');
+
+// solution.addEventListener('click', 
+//  function add() {
+//     let elementList = document.getElementsByClassName('hamburger-menu');
+//     elementList.style.display = 'inline-block'; 
+// } );
+
+
+
+element.addEventListener("click", function(){
+    let elementList = document.getElementsByClassName("hamburger-menu");
+    elementList.style.display = 'inline-block';
+  });
